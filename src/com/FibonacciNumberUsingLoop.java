d1512047febb469f66855b7296ec49554ad27d1a
pranavchoudhary <pranavchoudharyy@gmail.com>
2021-09-17 12:21:24 +0530

    Lecture - Fibonacci

---
+++ src/com/FibonacciNumberUsingLoop.java
@@ -0,0 +1,26 @@
+package com;
+
+import java.util.Scanner;
+
+public class FibonacciNumberUsingLoop
+{
+    public static void main(String[] args) {
+        int a =0;
+        int b =1;
+        int count = 2;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("which fibonacci term you want : ");
+        int n = scan.nextInt();
+
+        while(n>=count)
+        {
+            int temp = b;
+            b = b+a;
+            a = temp;
+            count++;
+        }
+
+        System.out.println(b);
+
+    }
+}
