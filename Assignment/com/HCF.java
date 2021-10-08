f2d29807c6d895c2c0e8b790da764b5553add7e1
pranavchoudhary <pranavchoudharyy@gmail.com>
2021-10-08 13:22:10 +0530

    Intermediate-20 Assignment - 03-Conditionals-Loops

--- Assignment/com/HCF.java
+++ Assignment/com/HCF.java
@@ -4,22 +4,27 @@ import java.util.Scanner;
 
 public class HCF {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the first Number ");
         int fNumber = scan.nextInt();
         System.out.println("Enter the second Number ");
         int sNumber = scan.nextInt();
-        if (sNumber < fNumber) {
+
+        if (sNumber < fNumber)
+        {
             int temp = sNumber;
             sNumber = fNumber;
             fNumber = temp;
         }
-        while (fNumber < sNumber) {
-            int rem = sNumber / fNumber;
-            sNumber = fNumber;
-            fNumber = rem;
-        }
-        System.out.println();
 
+        while (fNumber != 0 )
+        {
+            int temp = fNumber;
+            fNumber = sNumber%fNumber;
+            sNumber = temp;
+        }
+
+        System.out.println("HCF is : "+sNumber);
     }
 }
